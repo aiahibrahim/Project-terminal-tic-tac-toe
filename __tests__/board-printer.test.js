464966b11e3fd13034c01d7543aded4caff31eb3
aiah ibrahim <ayababikeribrahim@gmail.com>
2025-04-13 18:10:13 +0100

    test cases board printer

--- __tests__/board-printer.test.js
+++ __tests__/board-printer.test.js
@@ -1,6 +1,21 @@
-import { checkIfNoMovesLeft } from "../board-printer";
+// __tests__/board-printer.test.js
+
+const { checkIfNoMovesLeft } = require("../board-printer");
 
 test("checkIfNoMovesLeft should return true if there are no moves left", () => {
-  const board = [];
-  expect(checkIfNoMovesLeft(board)).toBe(true);
+    const board = [
+        ['X', 'O', 'X'],
+        ['X', 'O', 'O'],
+        ['O', 'X', 'X']
+    ];
+    expect(checkIfNoMovesLeft(board)).toBe(true);
+});
+
+test("checkIfNoMovesLeft should return false if there are moves left", () => {
+    const board = [
+        ['X', '_', 'X'],
+        ['X', 'O', 'O'],
+        ['O', 'X', 'X']
+    ];
+    expect(checkIfNoMovesLeft(board)).toBe(false);
 });
